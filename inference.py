be8a2502cd725300aef3fa669a5dabefa89e5f74
Daniel Klein <othercriteria@gmail.com>
2011-10-03 16:55:24 -0400

    Changed storage of statistics to avoid lots of gratuitious casts to array.

--- inference.py
+++ inference.py
@@ -46,32 +46,29 @@ def inference(params):
 
     # Precompute statistics
     print 'Precomputing statistics'
-    hits = [[]]
+    hits = [np.empty((n_w[0],)+theta_dim)]
     hits_observed = np.zeros(theta_dim)
     s_padded = np.zeros((params['N'],2*params['Delta']), dtype=np.bool)
-    for s in windows[0]:
+    for w, s in enumerate(windows[0]):
         s_padded[:,params['Delta']:(2*params['Delta'])] = s
-        hit = np.empty(theta_dim, dtype=np.uint32)
         for l in range(params['L']):
-            t_min, t_max = params['Delta']-(l+1), 2*params['Delta']-(l+1)
-            s_lagged = s_padded[:,t_min:t_max]
-            hit[:,:,l] = np.tensordot(s_lagged, s, axes = (1,1))
-        hits[0].append(hit)
+            tmin, tmax = params['Delta']-(l+1), 2*params['Delta']-(l+1)
+            s_lagged = s_padded[:,tmin:tmax]
+            hit = np.tensordot(s_lagged, s, axes = (1,1))
+            hits[0][w,:,:,l] = hit
     hits_observed += hits[0][0]
     for k in range(1, params['M']):
-        hits.append([])
-        for s_prev in windows[k-1]:
-            hits[k].append([])
+        hits.append(np.empty((n_w[k-1],n_w[k])+theta_dim))
+        for w_prev, s_prev in enumerate(windows[k-1]):
             s_padded[:,0:params['Delta']] = s_prev
-            for s in windows[k]:
+            for w, s in enumerate(windows[k]):
                 s_padded[:,params['Delta']:(2*params['Delta'])] = s
-                hit = np.empty(theta_dim, dtype=np.uint32)
                 for l in range(params['L']):
-                    t_min, t_max = params['Delta']-(l+1), 2*params['Delta']-(l+1)
-                    s_lagged = s_padded[:,t_min:t_max]
-                    hit[:,:,l] = np.tensordot(s_lagged, s, axes = (1,1))
-                hits[k][-1].append(hit)
-        hits_observed += hits[k][0][0]
+                    tmin, tmax = params['Delta']-(l+1), 2*params['Delta']-(l+1)
+                    s_lagged = s_padded[:,tmin:tmax]
+                    hit = np.tensordot(s_lagged, s, axes = (1,1))
+                    hits[k][w_prev,w,:,:,l] = hit
+        hits_observed += hits[k][0,0]
 
     # Common DP code used for likelihood and gradient calculations
     def dp(theta):
@@ -83,7 +80,7 @@ def inference(params):
             h[k] = np.empty((n_w[k-1], n_w[k]))
             for w_prev in range(n_w[k-1]):
                 for w in range(n_w[k]):
-                    h[k][w_prev,w] = np.sum(theta * hits[k][w_prev][w])
+                    h[k][w_prev,w] = np.sum(theta * hits[k][w_prev,w])
 
         b = [None] * (params['M']+1)
         b[params['M']] = np.zeros(n_w[params['M']-1])
@@ -116,15 +113,14 @@ def inference(params):
 
         # Compute expected statistics
         w_prob = unlog(h[0] + b[1])
-        hits_expected = fast_average(np.array(hits[0]), w_prob)
+        hits_expected = fast_average(hits[0], w_prob)
         for k in range(1, params['M']):
             w_prob_new = np.zeros(n_w[k])
             for w_prev in range(n_w[k-1]):
                 w_weight = unlog(h[k][w_prev,:] + b[k+1])
                 w_prob_new += w_weight * w_prob[w_prev]
                 hits_expected += (w_prob[w_prev] *
-                                  fast_average(np.array(hits[k][w_prev]),
-                                               weights=w_weight))
+                                  fast_average(hits[k][w_prev], w_weight))
             w_prob = w_prob_new
 
         return np.reshape(hits_expected - hits_observed, theta_vec.shape)
